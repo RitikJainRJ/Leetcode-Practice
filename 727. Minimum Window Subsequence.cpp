dde321eec0a6f0c4cee3795eb7ca26e2c3203605
Ritik Jain <ritikjain1002@gmail.com>
2020-06-16 13:44:46 +0530

    Update 727. Minimum Window Subsequence.cpp

--- 727. Minimum Window Subsequence.cpp
+++ 727. Minimum Window Subsequence.cpp
@@ -1,9 +1,10 @@
+#define ll unsigned long long
 class Solution {
 public:
     string minWindow(string &s, string &t) {
-        int n1 = s.length(), n2 = t.length();
+        int n1 = s.length(), n2 = t.length(), c, m = INT_MAX;
+        ll dp[n2 + 1][n1 + 1];
         string res;
-        int dp[n2 + 1][n1 + 1], c, m = INT_MAX;
 
         for(int i = 0; i <= n1; i++)
             dp[0][i] = 0;
@@ -11,28 +12,14 @@ public:
             dp[i][0] = INT_MAX;
         for(int i = 1; i <= n2; i++){
             for(int j = 1; j <= n1; j++){
-                dp[i][j] = dp[i][j - 1] != INT_MAX ? 1 + dp[i][j - 1] : dp[i][j - 1];
-                if(t[i - 1] == s[j - 1] && dp[i - 1][j - 1] != INT_MAX)
-                    dp[i][j] = min(dp[i][j], 1 + dp[i - 1][j - 1]);
-                if(i == n2 && m > dp[i][j]){
-                    m = dp[i][j];
-                    c = j;
-                }
+                dp[i][j] = t[i - 1] == s[j - 1] ? 1 + min(dp[i - 1][j - 1], dp[i][j - 1]) : 1 + dp[i][j - 1];
+                if(i == n2 && m > dp[i][j])
+                    m = dp[i][j], c = j;
             }
         }
-        if(dp[n2][n1] == INT_MAX)
+        if(m == INT_MAX)
             return res;
-        int i = n2, j = c, k = t.length();
-        while(k){
-            if(t[i - 1] == s[j - 1]){
-                if(dp[i][j - 1] > dp[i - 1][j - 1]){
-                    i--;
-                    k--;
-                }
-            }
-            j--;
-        }
-        res = s.substr(j, c - j);
+        res = s.substr(c - m, m);
         return res;
     }
 };
